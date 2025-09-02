defe0965b176cf5ba9620c503977287ae4c02905
清古拓聖 <241205095@ccmailg.meijo-u.ac.jp>
2025-09-02 11:56:19 +0900

    Add ImageView and update TextView
    This commit introduces an ImageView to the main activity layout and updates the existing TextView.
    
    - An ImageView with the ID `imageView` was added to `activity_main.xml`. It displays a drawable resource named `ic_android_fakered_24dp`.
    - The TextView with the ID `text` in `activity_main.xml` was modified:
        - Its text is now set from a string resource `@string/text`.
        - Rotation, rotationX, and rotationY attributes were added.
        - Text size was changed to 24sp.
    - A new vector drawable `ic_android_fakered_24dp.xml` was created.
    - A new string resource `text` with the value "Hello world！" was added to `strings.xml`.
    - In `MainActivity.java`:
        - View binding is now used (`ActivityMainBinding`).
        - The text of the `text` TextView is set programmatically using `binding.text.setText(R.string.text)`.
    - Gradle migration settings were updated in `.idea/gradle.xml`.
    - The XML declaration was removed from `.idea/misc.xml`.

--- app/src/main/java/jp/ac/meijou/android/s241205095/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205095/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.ac.meijou.android.s241205095;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import jp.ac.meijou.android.s241205095.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        binding.text.setText(R.string.text);
     }
 }
