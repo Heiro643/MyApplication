e54b438513eb4c88522496b4900dc01646df9ba3
清古拓聖 <241205095@ccmailg.meijo-u.ac.jp>
2025-09-03 16:20:47 +0900

    画面の遷移を実装しました 遷移時の値渡しを追加しました

--- app/src/main/java/jp/ac/meijou/android/s241205095/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205095/MainActivity2.java
@@ -1,25 +1,81 @@
 package jp.ac.meijou.android.s241205095;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Optional;
+
+import jp.ac.meijou.android.s241205095.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205095.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.returnTextView.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.returnTextView.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.returnTextView.setText("Result: Unknown(" + result.getResultCode() + ")");
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        //暗黙的
+        binding.button2.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
+        });
+        //明示的
+        binding.button3.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity3.class);
+            startActivity(intent);
+        });
+        binding.watpButton.setOnClickListener(view -> {
+            var text = binding.warpText.getText().toString();
+            var intent = new Intent(this, MainActivity4.class);
+            intent.putExtra("text", text);
+            startActivity(intent);
+        });
+        binding.button20.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
+        });
     }
+
 }
